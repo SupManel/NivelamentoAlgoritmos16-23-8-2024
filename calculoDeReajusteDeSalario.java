a154022a6520c71654f2c569cf508a9812c6160b
Emanuel Ramos Paiva <emanuelramospaiva@gmail.com>
2024-08-16 22:51:39 -0300

    Outros exercios de algoritmo

---
+++ calculoDeReajusteDeSalario.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class calculoDeReajusteDeSalario {
+    public static void main(String[] args) {
+        double salario;
+        Scanner prompt = new Scanner(System.in);
+        System.out.print("Informe seu salario: ");
+        salario = prompt.nextDouble();4
+        if (salario <= 1412) {
+            salario = salario * 0.12 + salario;
+        } else if (salario <= 3000) {
+            salario = salario * 0.09 + salario;
+        } else if (salario <= 5000) {
+            salario = salario * 0.05 + salario;
+        }else {
+            System.out.println("Sem alterações");
+        }
+        System.out.println("Salario: " + salario);
+    }
+}
